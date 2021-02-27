e136906026e7af195fd9b542361aaef2faa35520
lllqqqjjj <1224686994@qq.com>
2021-02-27 20:35:04 +0800

    head.h

---
+++ head.h
@@ -0,0 +1,10 @@
+#define LENGTH 30
+typedef struct User_login{
+    char name[LENGTH]; //用户名
+    int totalcount; //登录次数
+}SDataType; //链表的节点
+typedef struct SListNode
+{
+    SDataType data;
+    struct SListNode* PNext;
+}Node,*PNode; //封装链表节点和 next 指针
